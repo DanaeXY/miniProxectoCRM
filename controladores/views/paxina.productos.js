2427248b9287a6848a7b122705d83087bf164744
Fran <powebicreacion@gmail.com>
2025-03-18 17:43:42 +0100

    Primer css Fran

--- controladores/views/paxina.productos.js
+++
@@ -1,7 +0,0 @@
-const path = require("path");
-
-const paxinaProductos = (req,res)=>{
-    res.sendFile(path.join(__dirname,"../../dist/views/productos.html"))
-}
-
-module.exports = paxinaProductos
